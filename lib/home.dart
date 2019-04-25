c3a2ac7219c5d20b9d23305cce231c1b1071ea6f
ergofriend <altsir.cgc.yhyavr@gmail.com>
2019-04-25 16:12:52 +0900

    note

--- lib/home.dart
+++ lib/home.dart
@@ -1,25 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_tutorial/my_page.dart';
+import 'package:flutter_firebase_tutorial/timeline.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+
+  List<Widget> _bodyList = [
+    Timeline(),
+    MyPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: const Text(
-          "PhotoSNS",
-          style: const TextStyle(
-            color: Colors.black,
-          ),
-        ),
-      ),
+      // appBar: AppBar(
+      //   backgroundColor: Colors.white,
+      //   title: const Text(
+      //     "PhotoSNS",
+      //     style: const TextStyle(
+      //       color: Colors.black,
+      //     ),
+      //   ),
+      // ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
-        child: Icon(Icons.add), 
+        child: Icon(Icons.add),
       ),
-      body: Center(
-        child: const Text("Hello World"),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: _onTap,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text("ホーム"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Text("マイページ"),
+          ),
+        ],
       ),
+      body: _bodyList[_currentIndex], 
     );
+  }
+
+  _onTap(int i) {
+    setState(() {
+      _currentIndex = i;
+    });
   }
 }
